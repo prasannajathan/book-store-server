cba4836b1334941b46cdd8ee145587611da9d571
Prasanna Jathan <PrasannaAngara.Jathan@nespresso.com>
2020-12-04 18:20:36 +0100

    refactor: return data as object

--- routes/isTokenValid.js
+++ routes/isTokenValid.js
@@ -8,19 +8,24 @@ router.post('/is-token-valid', auth, async (req, res) => {
   try {
     const token = req.header('x-auth-token')
     if (!token) {
-      return res.json(false)
+      return res.json({ token: false })
     }
     // Verify Token
     const isVerified = jwt.verify(token, process.env.JWT_TOKEN)
     if (!isVerified) {
-      return res.json(false)
+      return res.json({ token: false })
     }
     // Find user
     const user = await User.findById(isVerified.id)
     if (!user) {
-      return res.json(false)
+      return res.json({ token: false })
     }
-    return res.json(true)
+    return res.json({
+      token: true,
+      firstName: user.firstName,
+      // eslint-disable-next-line no-underscore-dangle
+      id: user._id,
+    })
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
