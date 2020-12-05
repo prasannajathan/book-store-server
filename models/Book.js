8d34d17363431877bd8c401aa338f344651a9c06
Prasanna Jathan <PrasannaAngara.Jathan@nespresso.com>
2020-12-05 12:25:46 +0100

    refactor: (pagination) pull required fields

--- models/Book.js
+++ models/Book.js
@@ -6,6 +6,7 @@ const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String },
   price: { type: String },
+  created_at: { type: Date, default: Date.now },
 })
 
 bookSchema.plugin(mongoosePaginate)
--- routes/bookGet.js
+++ routes/bookGet.js
@@ -1,20 +1,18 @@
 const router = require('express').Router()
 
-
 const Book = require('../models/Book')
 
 router.get('/list', async (req, res) => {
   try {
-    const page = req.query.page || 0
+    const page = req.query.page || 1
     const limit = req.query.limit || 5
     const options = {
       page,
       limit,
+      select: ['title', 'price'],
     }
-
-    const lists = await Book.paginate({}, options)
-
-    res.json(lists)
+    const books = await Book.paginate({}, options)
+    res.json(books)
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
